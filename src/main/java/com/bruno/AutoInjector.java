cb73f45ba959a80920469115125f5a8cdf96544d
Bruno Moraes <moraes.7bruno@gmail.com>
2023-01-31 10:58:26 -0300

    added project to git

---
+++ src/main/java/com/bruno/AutoInjector.java
@@ -0,0 +1,260 @@
+package com.bruno;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+public class AutoInjector {
+
+    // Map of factory functions for creating instances of a certain type
+    private final Map<Class<?>, Supplier<?>> factoryMap = new HashMap<>();
+
+    // Map of singleton instances for a certain type
+    private final Map<Class<?>, Object> singletonMap = new HashMap<>();
+
+    // Map of lazy singleton factory functions for creating singleton instances of a certain type
+    private final Map<Class<?>, Supplier<?>> lazySingletonMap = new HashMap<>();
+
+    // Map of specific instances for a certain type
+    private final Map<Class<?>, Object> instanceMap = new HashMap<>();
+
+
+    /**
+     * Registers a factory function for creating instances of a certain type.
+     *
+     * @param supplier The factory function.
+     */
+    public <T> void add(Supplier<T> supplier) {
+        Class<T> type = getType(supplier);
+        factoryMap.put(type, supplier);
+    }
+
+    /**
+     * Registers a singleton instance for a certain type.
+     *
+     * @param supplier The factory function used to create the singleton instance.
+     */
+    public <T> void addSingleton(Supplier<T> supplier) {
+        Class<T> type = getType(supplier);
+        singletonMap.put(type, supplier.get());
+    }
+
+    /**
+     * Registers a lazy singleton factory function for creating singleton instances of a certain type.
+     *
+     * @param supplier The lazy singleton factory function.
+     */
+    public <T> void addLazySingleton(Supplier<T> supplier) {
+        Class<T> type = getType(supplier);
+        lazySingletonMap.put(type, supplier);
+    }
+
+    /**
+     * Registers a specific instance for a certain type.
+     *
+     * @param instance The instance to register.
+     */
+    public <T> void instance(T instance) {
+        instanceMap.put(instance.getClass(), instance);
+    }
+
+    /**
+     * Gets an instance of a certain type.
+     *
+     * @param type             The type of the instance to get.
+     * @param createIfNotFound If true, a new instance will be created using reflection if an
+     *                         instance of the given type is not found in the injector's maps.
+     * @return The instance, or null if not found and createIfNotFound is false.
+     */
+    public <T> T get(Class<T> type, boolean createIfNotFound) {
+
+        // Check if the instance is in the instanceMap
+        T instance = (T) instanceMap.get(type);
+        if (instance != null) {
+            return instance;
+        }
+
+        // Check if the instance is in the factoryMap
+        Supplier<T> supplier = (Supplier<T>) factoryMap.get(type);
+        if (supplier != null) {
+            return supplier.get();
+        }
+
+        // Check if the instance is in the singletonMap
+        T singleton = (T) singletonMap.get(type);
+        if (singleton != null) {
+            return singleton;
+        }
+
+        // Check if the instance is in the lazySingletonMap
+        Supplier<T> lazySingletonSupplier = (Supplier<T>) lazySingletonMap.get(type);
+        if (lazySingletonSupplier != null) {
+            T lazySingleton = lazySingletonSupplier.get();
+            singletonMap.put(type, lazySingleton);
+            return lazySingleton;
+        }
+
+        // If createIfNotFound is true, try to create the instance using reflection
+        if (createIfNotFound) {
+            try {
+                // Use reflection to get the constructor for the given type
+                Constructor<T> constructor = type.getConstructor();
+
+                // Get the parameter types for the constructor
+                Class<?>[] paramTypes = constructor.getParameterTypes();
+
+                // Create an array to store the constructor arguments
+                Object[] args = new Object[paramTypes.length];
+
+                // Recursively call get() for each required dependency
+                for (int i = 0; i < paramTypes.length; i++) {
+                    args[i] = get(paramTypes[i], true);
+                    if (args[i] == null) {
+                        // One or more dependencies were not found, so return null
+                        return null;
+                    }
+                }
+
+                // All dependencies were found, so create a new instance using the constructor
+                instance = constructor.newInstance(args);
+
+                // Add the instance to the instanceMap
+                instanceMap.put(type, instance);
+
+                return instance;
+            } catch (NoSuchMethodException | InstantiationException | IllegalAccessException |
+                     InvocationTargetException e) {
+                // The type does not have a no-arg constructor, so return null
+                return null;
+            } // An error occurred while trying to create the instance, so return null
+
+        }
+
+        return null;
+    }
+
+    /**
+     * Gets an instance of a certain type.
+     *
+     * @param type The type of the instance to get.
+     * @return The instance, or null if not found.
+     */
+    public <T> T get(Class<T> type) {
+        return get(type, false);
+    }
+
+    /**
+     * Gets an instance of a certain type using the default factory function.
+     *
+     * @return The instance.
+     * @throws IllegalArgumentException If the default factory function is not registered.
+     */
+    public <T> T get() {
+        return get(getType(null), true);
+    }
+
+    /**
+     * Request an instance by [Type]
+     */
+    public <T> T call(Class<T> type) {
+        return get(type);
+    }
+
+    /**
+     * Request an instance by [Type]
+     */
+    public <T> boolean isAdded() {
+        return isAdded(getType(null));
+    }
+
+    /**
+     * Checks if the instance record exists.
+     */
+    public boolean isAdded(Class<?> type) {
+        return factoryMap.containsKey(type) || singletonMap.containsKey(type) || lazySingletonMap.containsKey(type) || instanceMap.containsKey(type);
+    }
+
+    /**
+     * Helper function to get the type of an instance from a factory function.
+     */
+    private <T> Class<T> getType(Supplier<T> supplier) {
+        if (supplier == null) return null;
+        return (Class<T>) supplier.get().getClass();
+    }
+
+    /**
+     * Helper function to get the type of an instance from an object.
+     */
+    private <T> Class<T> getType(T instance) {
+        return (Class<T>) instance.getClass();
+    }
+
+    /**
+     * Inherit all instance and transform records.
+     */
+    public void addInjector(AutoInjector injector) {
+        factoryMap.putAll(injector.factoryMap);
+        singletonMap.putAll(injector.singletonMap);
+        lazySingletonMap.putAll(injector.lazySingletonMap);
+        instanceMap.putAll(injector.instanceMap);
+    }
+
+    /**
+     * Checks if the instance registration is as singleton.
+     */
+    public <T> boolean isInstantiateSingleton() {
+        return isInstantiateSingleton(getType(null));
+    }
+
+    /**
+     * Checks if the instance registration is as singleton.
+     */
+    public boolean isInstantiateSingleton(Class<?> type) {
+        return singletonMap.containsKey(type) || lazySingletonMap.containsKey(type);
+    }
+
+    /**
+     * Unregisters an instance by type.
+     */
+    public <T> void remove() {
+        remove(getType(null));
+    }
+
+    /**
+     * Unregisters an instance by type.
+     */
+    public void remove(Class<?> type) {
+        factoryMap.remove(type);
+        singletonMap.remove(type);
+        lazySingletonMap.remove(type);
+        instanceMap.remove(type);
+    }
+
+    /**
+     * Removes the singleton instance.
+     * This does not remove it from the registry tree.
+     */
+    public <T> void disposeSingleton() {
+        disposeSingleton(getType(null));
+    }
+
+    /**
+     * Removes the singleton instance.
+     * This does not remove it from the registry tree.
+     */
+    public void disposeSingleton(Class<?> type) {
+        singletonMap.remove(type);
+    }
+
+    /**
+     * Replaces an instance record with a concrete instance.
+     * This function should only be used for unit testing.
+     * Any other use is discouraged.
+     */
+    public <T> void replaceInstance(T instance) {
+        instanceMap.put(instance.getClass(), instance);
+    }
+
+}
---
+++ src/test/java/com/bruno/AutoInjectorTest.java
@@ -0,0 +1,150 @@
+package com.bruno;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AutoInjectorTest {
+
+    private AutoInjector autoInjector;
+
+    @BeforeMethod
+    public void setUp() {
+        autoInjector = new AutoInjector();
+    }
+
+    @Test
+    public void testAddSingleton() {
+        autoInjector.addSingleton(MyType::new);
+        MyType myType = autoInjector.get(MyType.class);
+        Assert.assertNotNull(myType);
+    }
+
+    @Test
+    public void testGet() {
+        autoInjector.add(MyType::new);
+        MyType myType = autoInjector.get(MyType.class);
+        Assert.assertNotNull(myType);
+    }
+
+    @Test
+    public void testRemove() {
+        autoInjector.addSingleton(MyType::new);
+        MyType myType = autoInjector.get(MyType.class);
+        Assert.assertNotNull(myType);
+
+        autoInjector.remove(MyType.class);
+
+        myType = autoInjector.get(MyType.class);
+        Assert.assertNull(myType);
+    }
+
+    @Test
+    public void testReplaceInstance() {
+        autoInjector.addSingleton(MyType::new);
+        MyType myObject = autoInjector.get(MyType.class);
+        Assert.assertNotNull(myObject);
+
+        MyType newMyObject = new MyType();
+        autoInjector.replaceInstance(newMyObject);
+        MyType replacedMyObject = autoInjector.get(MyType.class);
+        Assert.assertNotNull(replacedMyObject);
+
+        Assert.assertEquals(replacedMyObject, newMyObject);
+    }
+
+    @Test
+    public void testDisposeSingleton() {
+        autoInjector.addSingleton(MyType::new);
+        MyType myType = autoInjector.get(MyType.class);
+        Assert.assertNotNull(myType);
+
+        autoInjector.disposeSingleton(MyType.class);
+
+        myType = autoInjector.get(MyType.class);
+        Assert.assertNull(myType);
+    }
+
+    @Test
+    public void testIsInstantiateSingleton() {
+        autoInjector.addSingleton(MyType::new);
+        boolean isSingleton = autoInjector.isInstantiateSingleton(MyType.class);
+        Assert.assertTrue(isSingleton);
+    }
+
+    @Test
+    void testInstance() {
+        MyType myObject = new MyType();
+        autoInjector.instance(myObject);
+
+        Assert.assertNotNull(autoInjector.get(MyType.class));
+        Assert.assertEquals(myObject, autoInjector.get(MyType.class));
+    }
+
+    @Test
+    public void testAddLazySingleton() {
+        autoInjector.addLazySingleton(MyType::new);
+
+        MyType instance1 = autoInjector.get(MyType.class);
+        MyType instance2 = autoInjector.get(MyType.class);
+
+        // Test that the same instance is returned on subsequent calls to get
+        Assert.assertSame(instance1, instance2);
+    }
+
+    @Test
+    public void testCall() {
+        autoInjector.addSingleton(MyType::new);
+
+        // Test calling a registered instance
+        MyType myObject = autoInjector.call(MyType.class);
+        Assert.assertEquals(myObject.getValue(), "Hello World");
+
+        // Test calling an unregistered instance
+        try {
+            autoInjector.call(AnotherType.class);
+        } catch (IllegalStateException e) {
+            Assert.assertEquals(e.getMessage(), "Instance not found: AnotherType");
+        }
+
+        // Test calling a disposed instance
+        autoInjector.disposeSingleton(MyType.class);
+        try {
+            autoInjector.call(MyType.class);
+        } catch (IllegalStateException e) {
+            Assert.assertEquals(e.getMessage(), "Instance not found: MyType");
+        }
+    }
+
+    @Test
+    public void testIsAdded() {
+        autoInjector.add(MyType::new);
+
+        Assert.assertTrue(autoInjector.isAdded(MyType.class));
+        Assert.assertFalse(autoInjector.isAdded(AnotherType.class));
+    }
+
+    @Test
+    public void testAddInjector() {
+        AutoInjector injector1 = new AutoInjector();
+        AutoInjector injector2 = new AutoInjector();
+
+        injector1.addSingleton(MyType::new);
+        injector2.addSingleton(AnotherType::new);
+
+        injector1.addInjector(injector2);
+
+        Assert.assertTrue(injector1.isAdded(MyType.class));
+        Assert.assertTrue(injector1.isAdded(AnotherType.class));
+    }
+
+}
+
+class MyType {
+    public String getValue() {
+        return "Hello World";
+    }
+}
+
+class AnotherType {
+}
